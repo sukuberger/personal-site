d64e4be003eb0ff80913605352e0b0938e3b45ab
anonymous <>
2022-07-17 11:33:22 +0530

    update preference

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 This is my first website
 
+hosted at [sukuberger.github.io](https://sukuberger.github.io/personal-site)
+<!-- hosted at (sukubernetes.io) -->
--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,3 @@
-// Challenge 2:
-// Replace the arguments below according to your preference.
-
-// space, scary, military, romantic, cowboy, fantasy, superhero
-favouriteMovieGenre("space");
-
-// watermelon, tomato, banana, orange, avocado, blueberry
-favouriteFruit("avocado");
-
-// light, dark
-favouriteMode("dark");
-
-// sharp, soft, round
-favouriteEdgeStyle("soft");
-
 ////////////////////////////////////
 // IGONE THE CODE BELOW THIS LINE //
 ////////////////////////////////////
